bfc3e671cf75e3f38f9843f7b7cf7584319cf8a9
LexBarce <alexandriakowjieannedelossantos.barcelon24@bicol-u.edu.ph>
2025-12-28 17:54:28 +0800

    Welcome, Book List, and Borrow Form UI are completed

---
+++ src/tarabasalis/TaraBasaLIS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarabasalis;
+
+/**
+ *
+ * @author User
+ */
+public class TaraBasaLIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
